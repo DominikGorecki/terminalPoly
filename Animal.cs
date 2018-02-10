08cdedcbcce3f0723dd1b68e23472fe245229739
Dominik Gorecki <dom@nikgo.com>
2018-02-10 14:27:38 -0500

    Adding some examples of upcasting and downcasting

--- Animal.cs
+++ Animal.cs
@@ -1,4 +1,17 @@
+using System;
 namespace terminalPoly {
+
+    public static class AnimalHelpers {
+        /// <summary>
+        /// This static class illustrates polymorphism when
+        /// an instance of an object that inherits from the
+        /// animal class can be passed into this method
+        /// </summary>
+        public static void DisplayName(Animal animal) {
+            Console.WriteLine("Animal's name is: " + animal.Name);
+        }
+    }
+
     public class Animal {
         public Animal() {
             Population++;
@@ -7,6 +20,8 @@ namespace terminalPoly {
         public static int Population = 0;
     }
 
+    // example of inheritance. Horse also has he public property
+    // "Name" that is inherited from "Animal."
     public class Horse : Animal {
         public Horse() {
             HorsePopulation++;
--- Program.cs
+++ Program.cs
@@ -18,6 +18,24 @@ namespace terminalPoly
             Console.WriteLine("Population: " + Animal.Population);
             Console.WriteLine("Horse Population: " + Horse.HorsePopulation);
 
+            // Example of polymorphism--charlie is of type "Horse"
+            // and it can be passed into the static method that 
+            // accepts an "Animal" type parameter.
+            AnimalHelpers.DisplayName(charlie);
+
+            // Upcasting: charlieHorse is just a reference to charlie
+            // with a more limited scope
+            Animal charlieHorse = charlie;
+
+            Console.WriteLine("Is charlieHorse charlie? " + (charlie == charlieHorse));
+
+            // Downcasting:
+            Horse sameCharlie = (Horse)charlieHorse;
+
+            // Although charlieHorse was of type "Animal," we didn't lose any of the properties
+            // that were in the original, "charlie" since charlieHorse was just a reference. 
+            // However, if you tried to output the contents of charlieHourse.ManeColor, it wouldn't work!
+            Console.WriteLine(sameCharlie.ManeColor);
 
         }
     }
