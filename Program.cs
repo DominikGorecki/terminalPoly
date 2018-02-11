12c6ffbbd8895c1a0c2688f99f528357eb826109
Dominik Gorecki <dom@nikgo.com>
2018-02-11 13:38:25 -0500

    examples of the is and as operator

--- Program.cs
+++ Program.cs
@@ -62,14 +62,31 @@ namespace terminalPoly
             // just upcasted to the base class. That is, because henry was created as an "Animal"
             // we can't downcast him to to a Dog subclass. Check the static function that this calls.
             Dog henryDog = DowncastFromAnimal(henry);
-          
 
-                // However, if we know that there's a possibility of this occuring, we shouldn't use 
-                // catching exceptions for branching our code (for logic).
-                // We can, however, use the "as" operator, which evaluates to a null if the downcast fails.
-                Dog henryDogWithAs = henry as Dog;
 
-                //Console.WriteLine("henryDog is " + )"null");
+            // However, if we know that there's a possibility of this occuring, we shouldn't use 
+            // catching exceptions for branching our code (for logic).
+            // We can, however, use the "as" operator, which evaluates to a null if the downcast fails.
+            Dog henryDogWithAs = henry as Dog;
+
+            if(henryDogWithAs == null)
+                Console.WriteLine("henryDogWithAs is null");
+
+            // Or better yet we can check if we can even perform the downcasting with the is
+            // operator:
+
+            if(henry is Dog) 
+                Console.WriteLine("Downcasting henry to Dog is possible");
+            else
+                Console.WriteLine("Downcasting henry to Dog is not possible ");
+            
+            // Notice that this will work because although charlieHorse is of the Animal type, it was
+            // originally upcast from the Horse type and since upcasting still holds a refernce to the
+            // original instance, it can easily be downcast again. 
+            if(charlieHorse is Horse) 
+                Console.WriteLine("charlieHorse can be downcast to Horse and his mane color is: " + ((Horse)charlieHorse).ManeColor);
+
+
         }
     }
 }
